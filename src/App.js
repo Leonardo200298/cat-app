68b96e60c9da54a44d55c28a4a168593fb82e928
Leonardo <leonardosanchez119@gmail.com>
2022-06-29 20:47:01 -0300

    Feat:add content to github, first request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [allcats, setCats] = useState();
+
+  /*  setCats(jsonData) */
+  const jsonNoSe = async () => {
+    try {
+      const url = 'https://api.thecatapi.com/v1/breeds';
+      let promise = await axios.get(url);
+      console.log(promise)
+      let { data } = await promise;
+      console.log(data)
+      setCats(data)
+
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect( ()=>{
+    jsonNoSe()
+  }
+
+    
+, [])
+
   return (
     <div className="App">
-      <Routes/>
+      {/* Si o si tiene que a ver un map jajaja */}
+      {!allcats ? <h1>Loading...</h1> : allcats.map(cat => <p>{cat.name}</p>)}
+      
+
     </div>
   );
 }
